939b7c2dc1eb2086db4c1f0c806a450d567c8d44
Jeff <jeffkiks93@gmail.com>
2023-11-30 16:17:57 +0000

    changes

--- script.groovy
+++ script.groovy
@@ -1,21 +1,3 @@
-def buildJar() {
-    echo 'building the application ....'
-    sh 'mvn package'                                              
-}
-
-def buildImage() {
-    echo 'building the docker image ....'
-    withCredentials([usernamePassword(credentialsId: 'my-docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER' )]) {
-        sh 'docker build -t jkiks/demo-app:jma-2.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push jkiks/demo-app:jma-2.0'
-    }
-}
-
-def testApp() {
-    echo 'Testing the application ....'
-}
-
 def deployApp() {
     echo 'deploying the application ....'
 }
